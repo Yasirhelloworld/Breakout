a01062b53af630986d1fe40fbbe75809268020e5
S <s>
2013-05-11 11:55:34 -0400

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -156,7 +156,7 @@ public class Breakout extends GraphicsProgram {
 	 * 
 	 */
 	private boolean ballHitVerticalWall() {
-		if (BALL.getX() <= BALL_RADIUS || BALL.getX() >= (APPLICATION_WIDTH - BALL_RADIUS)) {
+		if (BALL.getX() <= 0 || BALL.getX() >= (APPLICATION_WIDTH - (BALL_RADIUS * 2))) {
 			return true;
 		} else {
 			return false;
@@ -167,7 +167,7 @@ public class Breakout extends GraphicsProgram {
 	 * 
 	 */
 	private boolean ballHitHorizontalWall() {
-		if (BALL.getY() <= BALL_RADIUS || BALL.getY() >= (APPLICATION_HEIGHT - BALL_RADIUS)) {
+		if (BALL.getY() <= 0 || BALL.getY() >= (APPLICATION_HEIGHT - (BALL_RADIUS * 2))) {
 			return true;
 		} else {
 			return false;
